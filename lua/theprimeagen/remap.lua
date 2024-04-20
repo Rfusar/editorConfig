fda9fe8f290b981c924ab0cadb8171db32784508
riccardo <rfusaro12@gmail.com>
2024-04-21 01:46:27 +0200

    0.0.0

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -3,7 +3,10 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.tabstop=1
-vim.opt.relativenumber=true
+
+
+
+
 
 vim.cmd("highlight Normal guibg=black")
 
@@ -16,8 +19,19 @@ local key = vim.keymap.set
 -- Explorer file
 key("n", "<C-b>", ":NvimTreeToggle<CR>", {silent=true})
 key("n", "<leader>ps", ":PackerSync<CR>", {silent=true})
+
+key("", "<leader>123", function() vim.opt.number = true end, {silent=true})
+key("", "<leader>101",  function() vim.opt.relativenumber = true end, {silent=true})
+
+
 -- comandi
 key("v", "<leader>c", '"+y')
+
+
+-- per configurare ovunque si è 
+key("", "<leader>cfg", function()  vim.cmd(":!x-terminal-emulator -e 'nvim ~/.config/nvim'")end)
+key("", "<leader>help", function() vim.cmd(":!x-terminal-emulator -e 'nvim ~/.config/nvim/documentazione.txt'")end)
+
 
 
 -- per salvare
