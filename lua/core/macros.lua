2fdcd3c4a8acae8c7cb0430a168f024fbb7fa57f
Rfusar <rfusaro12@gmail.com>
2024-09-15 19:33:36 +0200

    aggiornamenti generici

--- lua/core/macros.lua
+++ lua/core/macros.lua
@@ -13,13 +13,40 @@ local macros = {
 key('n', '<C-p>', ':lua SetMacros()<CR>', opts)
 key('', '<leader><leader>c', ':lua SetCharComment()<CR>', opts)
 key('', '<leader><leader>r', ':lua SetReplacement()<CR>', opts)
-key('v', '<leader>tag', ':lua setTagHTML()<CR>', {})
-
+key('n', '<leader>tag', ':lua setTagHTML()<CR>', {})
 
 function setTagHTML()
-    local input = vim.fn.input("Tag HTML: ")
-    local commands = 's<' .. input .. '></' .. input .. '><Esc>0f>p'
-    local termcodes = vim.api.nvim_replace_termcodes(commands, true, false, true)
+    local isReact = vim.fn.input("is React [y/n]: ")
+    isReact = (isReact == "y")
+
+    local tag = vim.fn.input("Tag HTML: ")
+    local id = vim.fn.input("Id: ")
+    local class = vim.fn.input("Class: ")
+    local tabs = tonumber(vim.fn.input("Tabs: ")) or 1
+
+
+    local stringa = '<' .. tag
+
+    if id ~= "" then stringa = stringa .. ' id="' .. id .. '"' end
+
+    if class ~= "" then
+        local c = ""
+        if isReact then c = ' className="'
+        else c = ' class="' end
+        stringa = stringa .. c .. class .. '"'
+    end
+
+    if stringa:sub(-1) ~= '>' then stringa = stringa .. '>' end
+
+    stringa = stringa .. '</' .. tag .. '>'
+    vim.api.nvim_put({stringa}, 'c', true, true)
+
+    -- Posiziona il cursore all'interno del TagHtml
+    local text = "hbhhi<CR><CR><Esc>ki"
+    -- add Tabs
+    for t=1, tabs do text = text .. "<Tab>" end
+
+    local termcodes = vim.api.nvim_replace_termcodes(text, true, false, true)
     vim.api.nvim_feedkeys(termcodes, 'n', true)
 end
 
--- lua/core/plugin/autocompletamento.lua
+++ lua/core/plugin/autocompletamento.lua
@@ -3,7 +3,6 @@ local cmp = require'cmp'
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       require('luasnip').lsp_expand(args.body)  -- For `luasnip` users.
     end,
