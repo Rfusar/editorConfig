5702a4e7ad55511cf98082545b5211fb158afab1
Rfusar <rfusaro12@gmail.com>
2025-05-23 21:17:51 +0200

    23-05-2025

--- lua/core/plugin/init.lua
+++ lua/core/plugin/init.lua
@@ -4,12 +4,13 @@ require("core.plugin.color")
 require("core.plugin.autocompletamento")
 require("core.plugin.lettori")
 require("core.plugin.comments")
+local C = require("core.console.colors")
 
 --GITHUB
 function pushGithub(branch, namespace, comment)
     -- Validate commit message
     if comment == "" then
-        print("Commit message cannot be empty!")
+        C.SetColors("Commit message cannot be empty!", "Danger")
         return
     end
     -- Escape quotes in commit message
@@ -18,23 +19,23 @@ function pushGithub(branch, namespace, comment)
     -- Execute git commands with error handling
     local add_result = vim.fn.system('git add .')
     if vim.v.shell_error ~= 0 then
-        print("Error adding files: "..add_result)
+        C.SetColors("Error adding files: "..add_result, "Danger")
         return
     end
     
     local commit_result = vim.fn.system('git commit -m "'..comment..'"')
     if vim.v.shell_error ~= 0 then
-        print("Error committing: "..commit_result)
+        C.SetColors("Error committing: "..commit_result, "Danger")
         return
     end
     
     local push_result = vim.fn.system('git push '..namespace..' '..branch)
     if vim.v.shell_error ~= 0 then
-        print("Error pushing: "..push_result)
+        C.SetColors("Error pushing: "..push_result, "Danger")
         return
     end
     
-    print("Successfully pushed to "..namespace.."/"..branch)
+    C.SetColors("Successfully pushed to "..namespace.."/"..branch, "Success")
 end
 
 vim.api.nvim_create_user_command(
