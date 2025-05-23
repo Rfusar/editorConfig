27664bdea82d54a9bca562af2362e23d07b3ca9f
Rfusar <rfusaro12@gmail.com>
2025-05-24 00:58:53 +0200

    24-05-2025

--- lua/core/plugin/init.lua
+++ lua/core/plugin/init.lua
@@ -16,7 +16,7 @@ local C = require("core.console.colors")
 function pushGithub(branch, namespace, comment)
     -- Validate commit message
     if comment == "" then
-        N.myLog("Commit message cannot be empty!", "error", {timeout=10000})
+        N.myLog("Commit message cannot be empty!", "error")
         return
     end
     -- Escape quotes in commit message
@@ -25,23 +25,23 @@ function pushGithub(branch, namespace, comment)
     -- Execute git commands with error handling
     local add_result = vim.fn.system('git add .')
     if vim.v.shell_error ~= 0 then
-        N.myLog("Error adding files: "..add_result, "error", {timeout=60000})
+        N.myLog("Error adding files: "..add_result, "error")
         return
     end
     
     local commit_result = vim.fn.system({ "git", "commit", "-m", comment })
     if vim.v.shell_error ~= 0 then
-        N.myLog("Error committing: "..commit_result, "warning", {timeout=60000})
+        N.myLog("Error committing: "..commit_result, "error")
         return
     end
     
     local push_result = vim.fn.system({"git", "push", namespace, branch})
     if vim.v.shell_error ~= 0 then
-        N.myLog("Error pushing: "..push_result, "error", {timeout=60000})
+        N.myLog("Error pushing: "..push_result, "error")
         return
     end
     
-    N.myLog("Successfully pushed to "..namespace.."/"..branch, "info", {timeout=500})
+    N.myLog("Successfully pushed to "..namespace.."/"..branch, "info")
 end
 
 vim.api.nvim_create_user_command(
