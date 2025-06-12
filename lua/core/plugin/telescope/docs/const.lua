8116baa879254e21c36626c37f8e2523bc8b6b55
Rfusar <rfusaro12@gmail.com>
2025-06-12 03:39:51 +0200

    12-06-2025

--- lua/core/plugin/telescope/docs/const.lua
+++ lua/core/plugin/telescope/docs/const.lua
@@ -4,6 +4,9 @@ M.DOC_LINKS = {
     ["KTA"] = {
         ["CaptureDocumentService"] = "https://docshield.tungstenautomation.com/KTA/en_US/8.1.0-rmx0b1ux3q/help/SDK_Documentation/latest/class_agility_1_1_sdk_1_1_services_1_1_capture_document_service.html#a135da9da7d3cae63b4e6df2da579eeba"
     },
+    ["git"] = {
+        ["book"] = "https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control",
+    },
     ["KCM"] = {
         ["main_docs"] = "https://docshield.kofax.com/KCM/en_US/5.8.0-jx2hss7wd8/print/KofaxCommunicationsManagerTemplateScriptingLanguageDevelopersGuide_EN.pdf",
     },
