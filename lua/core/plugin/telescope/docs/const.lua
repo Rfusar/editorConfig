01f2203488a5165495be6bc9eb16fcf24223b45f
Rfusar <rfusaro12@gmail.com>
2025-07-17 13:44:00 +0200

    17-07-2025

--- lua/core/plugin/telescope/docs/const.lua
+++ lua/core/plugin/telescope/docs/const.lua
@@ -5,6 +5,7 @@ M.DOC_LINKS = {
         ["GENERAL 7.5"] = "https://docshield.tungstenautomation.com/KTA/en_US/750-4kcae04o43/help/TA/TotalAgility_help/c_welcome.html",
         ["CaptureDocumentService"] = "https://docshield.tungstenautomation.com/KTA/en_US/8.1.0-rmx0b1ux3q/help/SDK_Documentation/latest/class_agility_1_1_sdk_1_1_services_1_1_capture_document_service.html#a135da9da7d3cae63b4e6df2da579eeba",
         ["General 8.1"] = "https://docshield.tungstenautomation.com/KTA/en_US/8.1.0-rmx0b1ux3q/help/TotalAgilityApps/TotalAgility_apps/c_TAappintro.html",
+        ["How force unlock things"] = "https://partner.kofax.com/s/case/500Pr00000QpjFXIAZ/exctration-group-locked",
     },
     ["GIT"] = {
         ["GENERAL"] = "https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control",
@@ -18,29 +19,35 @@ M.DOC_LINKS = {
         ["GENERAL"] = "https://docs.python.org/3/",
         ["sqlite3"] = "https://docs.python.org/3/library/sqlite3.html"
     },
+    ["TYPESCRIPT"] = {
+        ["tsconfig"] = "https://www.typescriptlang.org/tsconfig/#references",  
+        ["general"] = "https://www.typescriptlang.org/docs/",
+    },
     ["OTHER LANG DOCS"] = {
         ["C#"] = "https://learn.microsoft.com/en-us/dotnet/csharp/",
         ["System Namespace .NET"] = "https://learn.microsoft.com/en-us/dotnet/api/system?view=net-9.0",
         ["Golang"] = "https://pkg.go.dev/std",
+        ["javascript"] = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference",
     },
     ["WEB PAGE"] = {
         ["tailwind"] = "https://tailwindcss.com/docs/installation/using-vite",
         ["nextjs"] = "https://nextjs.org/",
         ["react hooks"] = "https://react.dev/reference/react",
-        ["javascript"] = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference",
     },
     ["TECHNOLOGIES"] = {
         ["SYS - docker"] = "https://docs.docker.com/reference/cli/docker/",
+        ["Dockerfile references"] = "https://docs.docker.com/reference/dockerfile/",
         ["SYS - kubernetes"] = "https://kubernetes.io/it/docs/",
-        ["DB - sqlite"] = "https://www.sqlite.org/lang.html",
-        ["DB - postgres"] = "https://www.postgresql.org/docs/current/",
-        ["DB - MongoDB"] = "https://www.mongodb.com/docs/manual/core/document/?msockid=08b4379427326cfa1fc4219b26136def",
+        ["DB  - sqlite"] = "https://www.sqlite.org/lang.html",
+        ["DB  - postgres"] = "https://www.postgresql.org/docs/current/",
+        ["DB  - MongoDB"] = "https://www.mongodb.com/docs/manual/core/document/?msockid=08b4379427326cfa1fc4219b26136def",
 
     }, 
     ["AI"] = {
         ["chatGPT"] = "https://chatgpt.com/",
         ["deepseek"] = "https://chat.deepseek.com",
     },
+
     -- FUNCS
     ["Add New Doc Page"] = "",
 }
